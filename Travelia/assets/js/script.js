0c54a1242153ce1ee633f525601ac94e13bea2b4
Marwamu <marwamohamed1293@gmail.com>
2023-12-17 18:00:18 +0200

    Navbar Mobile

--- Travelia/assets/js/script.js
+++ Travelia/assets/js/script.js
@@ -3,3 +3,16 @@ const preloader = document.querySelector("[data-preloader]");
 window.addEventListener("load", () => {
   preloader.classList.add("remove");
 });
+
+// add event on multiple elements
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < 0; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+};
+
+// Navbar toogle for mobile
+
+const navBar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
